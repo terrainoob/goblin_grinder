1bee8bf49d56c883d8ad05450c93785a71c82af7
terrainoob <terrainoob@gmail.com>
2022-09-04 17:42:55 -0400

    add basic Sorcery setup for OAuth

--- Gemfile
+++ Gemfile
@@ -3,17 +3,18 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.2"
 
-gem "rails", "~> 7.0.3", ">= 7.0.3.1"
-gem "sprockets-rails"
-gem "pg", "~> 1.1"
-gem "puma", "~> 5.0"
-gem "jsbundling-rails"
-gem "turbo-rails"
-gem "stimulus-rails"
+gem "bootsnap", require: false
 gem "cssbundling-rails"
 gem "jbuilder"
+gem "jsbundling-rails"
+gem "pg", "~> 1.1"
+gem "puma", "~> 5.0"
+gem "rails", "~> 7.0.3", ">= 7.0.3.1"
+gem "sorcery"
+gem "sprockets-rails"
+gem "stimulus-rails"
+gem "turbo-rails"
 gem "tzinfo-data", platforms: %i[mingw mswin x64_mingw jruby]
-gem "bootsnap", require: false
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_04_214134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
 
 end
