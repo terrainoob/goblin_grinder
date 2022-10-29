fa86c6debae7e89a6728cb56b70bee04648efe50
rebuilt <memoryman51@hotmail.com>
2022-10-28 18:45:40 -0700

    Modified index_spec.rb to pass tests. Run tests with `bundle exec rspec`.  Reset rails_helper to default.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ require_relative '../config/environment'
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'simplecov'
 require 'rspec/rails'
-require 'phlex/test_helpers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,8 +31,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  config.include Phlex::TestHelpers, type: :view
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
