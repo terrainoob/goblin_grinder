4c43ea5b858013a4539f7c9ce5b1d87a28d34b35
terrainoob <terrainoob@gmail.com>
2022-11-10 12:58:39 -0500

    fix failing test that we forgot to resolve last time. minor stuff

--- spec/views/notes/index_spec.rb
+++ spec/views/notes/index_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe Views::Notes::Index do
-
-  let(:output) { render Views::Notes::Index.new }
+  let(:output) { render Views::Notes::Index.new(notes: Note.all) }
 
   it 'prints hello world' do
     expect(output).not_to be_nil
